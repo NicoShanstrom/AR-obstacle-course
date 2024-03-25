8c4378f71ff68ae29476d6586eaa4dfd023ba598
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-03-25 15:57:05 -0400

    Complete week 1

--- spec/models/aroc_week_1_spec.rb
+++ spec/models/aroc_week_1_spec.rb
@@ -33,13 +33,12 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
 
   it '2. finds order id of smallest order' do
     # ----------------------- Using Raw SQL ----------------------
-    # result = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount ASC LIMIT 1')
-    # order_id = result.first[:id]
+      # order_id = ActiveRecord::Base.connection.execute('SELECT id FROM orders ORDER BY amount ASC LIMIT 1').first['id']
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
-    # order_id = Order.select(:id).order(:amount).first.id
+    order_id = Order.select(:id).order(:amount).first.id
    
     # Your solution should not contain the actual ID of the order anywhere.
     # ------------------------------------------------------------
@@ -90,11 +89,12 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
     expected_objects = [@item_1, @item_4, @item_2]
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all.select { |item| ids_to_find.include?(item.id) }
+    # items = Item.all.select { |item| ids_to_find.include?(item.id) }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # 
+      # items = Item.where("id = #{@item_1.id} or id = #{@item_2.id} or id = #{@item_4.id}")
+      items = Item.where(id: ids_to_find)
     # ------------------------------------------------------------
 
     # Expectation
@@ -105,11 +105,11 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
     ids_to_find = [@order_1.id, @order_3.id, @order_5.id, @order_7.id]
 
     # ----------------------- Using Ruby -------------------------
-    orders = Order.all.select { |order| ids_to_find.include?(order.id) }
+    # orders = Order.all.select { |order| ids_to_find.include?(order.id) }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    orders = Order.where(id: ids_to_find)
     # ------------------------------------------------------------
 
     # Expectation
@@ -119,11 +119,12 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
   it '7. finds orders with an amount between 700 and 1000' do
     expected_result = [@order_11, @order_13, @order_8, @order_10, @order_15, @order_14, @order_12]
     # ----------------------- Using Ruby -------------------------
-    orders_between_700_and_1000 = Order.all.select { |order| order.amount >= 700 && order.amount <= 1000 }
+    # orders_between_700_and_1000 = Order.all.select { |order| order.amount >= 700 && order.amount <= 1000 }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+      orders_between_700_and_1000 = Order.where(amount: 700..1000)
+      # orders_between_700_and_1000 = Order.where("amount > 700 AND amount < 1000").to_a ##i dont get why this doesnt work
     # ------------------------------------------------------------
 
     # Expectation
@@ -134,11 +135,11 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
     expected_result = [@order_3, @order_2, @order_1, @order_4]
 
     # ----------------------- Using Ruby -------------------------
-    orders_less_than_550 = Order.all.select { |order| order.amount < 550 }
+    # orders_less_than_550 = Order.all.select { |order| order.amount < 550 }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    orders_less_than_550 = Order.where("amount < ?", 550)
     # ------------------------------------------------------------
 
     # Expectation
